e6858fbd50b722086305ca00b2dfe0a788bbfdb0
Marko Mikulicic <mmikulicic@gmail.com>
2021-08-20 15:57:39 +0200

    Install hook only when heap profiler is enabled

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod profiler;
 pub use profiler::*;
 
 mod collector;
-#[cfg(feature = "jemalloc_shim")]
+#[cfg(feature = "enable_heap_profiler")]
 mod hook;
 
 #[cfg(feature = "jemalloc_shim")]
@@ -16,7 +16,7 @@ use jemalloc_adapter as adapter;
 // module won't override the respective weak symbols from libc, since they don't ever get linked in the final executable.
 // On macos this is not necessary, but it doesn't hurt.
 // (e.g. the functions that override weak symbols exported by libc)
-#[cfg(feature = "jemalloc_shim")]
+#[cfg(feature = "enable_heap_profiler")]
 pub fn dummy_force_link() {
     hook::dummy_force_link();
 }
